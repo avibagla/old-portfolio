e59e92717125a8e39e26e08c88b4669f62b93527
Avi Bagla <avibagla@gmail.com>
2015-01-24 22:41:48 -0800

    got the home page done

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 // Foundation JavaScript
 // Documentation can be found at: http://foundation.zurb.com/docs
 $(document).foundation();
+
+$(document).ready(function(){
+	$('.projects').mouseover(function(){
+		$(this).children('img').css('-webkit-filter', 'none');
+	});
+	$('.projects').mouseout(function(){
+		$(this).children('img').css('-webkit-filter', 'blur(15px)');
+	});
+});
